144555a9514edc6e2f8740b96fd7ee92aa01d4fc
yshrsmz <the.phantom.bane+github@gmail.com>
2023-02-20 21:12:18 +0900

    add BKTStorage and DefaultStorage implementation

---
+++ src/internal/storege/index.ts
@@ -0,0 +1,32 @@
+
+export const keyname = (prefix: string, key: string): string => `${prefix}_${key}`
+
+export interface BKTStorage<T> {
+  set(value: T | null): void
+  get(): T | null
+  clear(): void
+}
+
+export class DefaultStorage<T> implements BKTStorage<T> {
+  private key: string
+
+  constructor(key: string) {
+    this.key = key
+  }
+
+  public set<T>(value: T | null) {
+    localStorage.setItem(this.key, JSON.stringify(value))
+  }
+
+  public get<T>(): T | null {
+    const result = localStorage.getItem(this.key)
+    if (result === null) {
+      return null
+    }
+    return JSON.parse(result) as T
+  }
+
+  public clear() {
+    localStorage.removeItem(this.key)
+  }
+}
