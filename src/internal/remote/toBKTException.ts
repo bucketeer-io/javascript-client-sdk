32be4de31daf4d5e52491311e18b140db7ee041c
duyhungtnn <duyhung71089@gmail.com>
2024-04-15 10:28:35 +0700

    fix: update logic to convert status code to BKTException

--- src/internal/remote/toBKTException.ts
+++ src/internal/remote/toBKTException.ts
@@ -41,40 +41,37 @@ export const toBKTException = async (
   const { message = responseText } = errorBody?.error ?? {}
 
   const status = response.status
-  switch (status) {
-    case 400:
+  switch (true) {
+    case status >= 300 && status < 400:
+      return new RedirectRequestException(
+        response.status,
+        message ?? 'Redirect Request',
+      )
+    case status == 400:
       return new BadRequestException(message ?? 'Bad Request')
-    case 401:
+    case status == 401:
       return new UnauthorizedException(message ?? 'Unauthorized')
-    case 403:
+    case status == 403:
       return new ForbiddenException(message ?? 'Forbidden')
-    case 404:
+    case status == 404:
       return new NotFoundException(message ?? 'Feature Not Found')
-    case 405:
+    case status == 405:
       return new InvalidHttpMethodException(message ?? 'Invalid HTTP Method')
-    case 408:
+    case status == 408:
       return new TimeoutException(0, message ?? 'Timeout with status 408')
-    case 413:
+    case status == 413:
       return new PayloadTooLargeException(message ?? 'Payload Too Large')
-    case 499:
+    case status == 499:
       return new ClientClosedRequestException(
         message ?? 'Client Closed Request',
       )
-    case 500:
+    case status == 500:
       return new InternalServerErrorException(
         message ?? 'Internal Server Error',
       )
-    case 502:
-    case 503:
-    case 504:
+    case [502, 503, 504].includes(status):
       return new ServiceUnavailableException(message ?? 'Service Unavailable')
     default:
-      if (status >= 300 && status < 400) {
-        return new RedirectRequestException(
-          response.status,
-          message ?? 'Redirect Request',
-        )
-      }
       return new UnknownException(
         `Unknown Error: ${response.status} ${response.statusText}, ${responseText} ,${JSON.stringify(errorBody)}`,
         response.status,
