307fbbb00c39b450a756ef38ca9a64853df213ff
Kenta Kozuka <kenta.kozuka@gmail.com>
2024-07-12 11:04:39 +0900

    use flat config for eslint
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

---
+++ eslint.config.cjs
@@ -0,0 +1,59 @@
+const eslintConfigPrettier = require('eslint-config-prettier');
+const tsParser = require('@typescript-eslint/parser');
+const tsEslint = require('@typescript-eslint/eslint-plugin');
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  {
+    files: ['**/*.ts', 'src/**/*.js', '**/*.cjs'],
+  },
+  {
+		ignores: [
+			'dist',
+			'eslint.config.cjs',
+			'node_modules',
+			'tsconfig.json',
+		]
+  },
+  ...tseslint.configs.recommended,
+	eslintConfigPrettier,
+  {
+    languageOptions: {
+      parser: tsParser,
+    }
+  },
+	{
+		plugins: {
+      "@typescript-eslint": tsEslint,
+    },
+		rules: {
+			'no-multiple-empty-lines': 'error',
+			quotes: ['error', 'single', { allowTemplateLiterals: true }],
+			semi: 'off',
+			'@typescript-eslint/semi': ['error', 'never'],
+			'@typescript-eslint/member-delimiter-style': [
+				'error',
+				{
+					multiline: {
+						delimiter: 'none',
+						requireLast: true,
+					},
+					singleline: {
+						delimiter: 'semi',
+						requireLast: false,
+					},
+					multilineDetection: 'brackets',
+				},
+			],
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+				},
+			],
+		},
+	}
+];
