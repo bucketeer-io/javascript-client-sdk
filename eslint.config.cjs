6f4091258040506c98065cf520fe4f3192358cb7
duyhungtnn <duyhung71089@gmail.com>
2025-08-06 09:36:19 +0700

    chore: add custom ESLint rule and update config
    Introduces a custom ESLint rule 'no-spread-after-defaults' to disallow spreading objects after default properties in object literals. Updates eslint.config.cjs to include the new rule, expands ignore patterns, and sets parser options. Upgrades @typescript-eslint/eslint-plugin and @typescript-eslint/parser to version 8.39.0. Updates tsconfig.json to enable strictNullChecks and expand included files.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,48 +1,61 @@
-const eslintConfigPrettier = require('eslint-config-prettier');
-const tsParser = require('@typescript-eslint/parser');
-const tsEslint = require('@typescript-eslint/eslint-plugin');
-const tseslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier')
+const tsParser = require('@typescript-eslint/parser')
+const tsEslint = require('@typescript-eslint/eslint-plugin')
+const tseslint = require('typescript-eslint')
 
 module.exports = [
-	{
-		files: ['**/*.ts', 'src/**/*.js', '**/*.cjs'],
-	},
-	{
-		ignores: [
-			'dist',
-			'eslint.config.cjs',
-			'node_modules',
-			'tsconfig.json',
-		]
-	},
-	...tseslint.configs.recommended,
-	eslintConfigPrettier,
-	{
-		languageOptions: {
-			parser: tsParser,
-		}
-	},
-	{
-		rules: {
-			'no-multiple-empty-lines': 'error',
-			quotes: ['error', 'single', { allowTemplateLiterals: true }],
-			semi: ['error', 'never'],
-			'@typescript-eslint/no-unused-vars': [
-				'warn',
-				{
-					argsIgnorePattern: '^_',
-					varsIgnorePattern: '^_',
-					caughtErrorsIgnorePattern: '^_',
-					destructuredArrayIgnorePattern: '^_',
-				},
-			],
-			'@typescript-eslint/no-unused-expressions': [
-				'error',
-				{
-					allowShortCircuit: true,
-					allowTernary: true,
-				},
-			],
-		},
-	}
-];
+  {
+    files: ['**/*.ts', 'src/**/*.js', '**/*.cjs'],
+  },
+  {
+    ignores: [
+      'dist',
+      'eslint.config.cjs',
+      'node_modules',
+      'tsconfig.json',
+      '**/*.d.ts',
+    '.github',
+    'eslint-rules/',
+    ],
+  },
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    plugins: {
+      'custom-rules': require('./eslint-rules/no-spread-after-defaults.js').default,
+    },
+    rules: {
+      'no-multiple-empty-lines': 'error',
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      semi: ['error', 'never'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+      '@typescript-eslint/prefer-nullish-coalescing': ['error'],
+      'custom-rules/no-spread-after-defaults': 'error',
+    },
+  },
+]
